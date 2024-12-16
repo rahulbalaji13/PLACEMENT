ca56edfe13f5e8f12e68cab3bbe32761f9a46c34
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2024-12-16 09:44:58 +0530

    Create mergeStringAlternately.java

---
+++ LeetCode75Series/mergeStringAlternately.java
@@ -0,0 +1,60 @@
+/* Merge the string in the alternative: 
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+Example 1:
+
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+Example 2:
+
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+Example 3:
+
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d*/
+
+/* IMPORTANT: Make use of StringBuilder class in java for string problem */
+
+class Solution 
+{
+    public String mergeAlternately(String word1, String word2) 
+    {
+         StringBuilder result = new StringBuilder();
+         int i = 0;
+         int j = 0; 
+         
+         while(i < word1.length() && j < word2.length())
+         {
+             result.append(word1.charAt(i));
+             result.append(word2.charAt(i));
+             i++;
+             j++;
+         }
+         
+         while(i < word1.length())
+         {
+             result.append(word1.charAt(i++));
+         }   
+
+         while(j < word2.length())
+         {
+             result.append(word2.charAt(j++));
+         }
+          return result.toString(); 
+    }
+}
