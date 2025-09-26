1a9139c28a64fd7970ec7ca24e20a88732082c5c
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2025-09-26 08:31:16 +0530

    Create revNum&Str.cpp

---
+++ Roadmap_to_CPP/Foundations/revNum&Str.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[] = {1,2,3,4,5};
+    
+    int minVal = a[0];
+    int maxVal = a[0];
+    
+    for(int i = 0; i < 5; i++)
+    {
+        if(a[i] < minVal)
+        {
+            minVal = a[i];
+        }
+        else if(a[i] > maxVal)
+        {
+            maxVal = a[i];
+        }
+    }
+    cout<<"Min is: "<<minVal<<endl;
+    cout<<"Max is: "<<maxVal<<endl;
+    return 0;
+}
+
+
+-----------------------------------------
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "HELLO WORLD";
+    reverse(s.begin(),s.end());
+    cout<<s<<" ";
+ 
+    return 0;
+}
