677ca816a20c4612c88178fe84cf5789b79721b3
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2025-09-28 15:56:07 +0530

    Update and rename maxmin.cpp to foundationalCode.cpp

--- Roadmap_to_CPP/Foundations/foundationalCode.cpp
+++ Roadmap_to_CPP/Foundations/foundationalCode.cpp
@@ -25,32 +25,6 @@ int main()
     return 0;
 }
 
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int a[] = {1,2,3,4,5};
-    
-    int minVal = a[0];
-    int maxVal = a[0];
-    
-    for(int i = 0; i < 5; i++)
-    {
-        if(a[i] < minVal)
-        {
-            minVal = a[i];
-        }
-        else if(a[i] > maxVal)
-        {
-            maxVal = a[i];
-        }
-    }
-    cout<<"Min is: "<<minVal<<endl;
-    cout<<"Max is: "<<maxVal<<endl;
-    return 0;
-}
-
-
 //2. REVERSE THE GIVEN STRING
 
 #include<bits/stdc++.h>
