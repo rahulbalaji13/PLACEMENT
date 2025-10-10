2dfb963a08a35260b7c25aa597d99e88210c888f
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2025-10-10 12:05:17 +0530

    Update foundationalCode.cpp

--- Roadmap_to_CPP/Foundations/foundationalCode.cpp
+++ Roadmap_to_CPP/Foundations/foundationalCode.cpp
@@ -248,7 +248,43 @@ int main()
 }
 
 //Rotate an array by k position
+//Rotate the element in array for k positions
 
+#include<bits/stdc++.h>
+using namespace std;
+void rotatekpos(int arr[], int n, int k)
+{
+    k = k % n;
+    int temp[n];
+    
+    for(int i = 0; i < k; i++)
+    {
+        temp[i] = arr[n - k + i];
+    }
+    
+    for(int i = k; i < n; i++)
+    {
+        temp[i] = arr[i - k];
+    }
+    
+    for(int i = 0; i < n; i++)
+    {
+        arr[i] = temp[i];
+    }
+}
+
+int main()
+{
+   int arr[] = {1,2,3,4,7,8,9};
+   int k = 2;
+   int n = sizeof(arr)/sizeof(arr[0]);
+   rotatekpos(arr, n, k);
+   for(int i = 0; i < n; i++)
+   {
+       cout<<arr[i]<<" ";
+   }
+   return 0;
+}
 
 
 
