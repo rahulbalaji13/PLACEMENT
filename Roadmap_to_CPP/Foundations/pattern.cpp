94ad6acf67497a1eedb34546e5b90c4664ebd5de
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2025-10-18 12:40:20 +0530

    Create pattern.cpp

---
+++ Roadmap_to_CPP/Foundations/pattern.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  for(int i = 0; i < 5; i++)
+  {
+    for(int j = 0; j < 5; j++)
+    {
+      cout<<"*";    
+    }
+    cout<<endl;
+  }
+   return 0;
+}  
+
+/**
+OUTPUT:
+
+*****
+*****
+*****
+*****
+*****
+
+**/
+
