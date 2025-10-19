a0e1d1a8e2aec67946ff8e51a8fb405752e4d309
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2025-10-19 16:19:20 +0530

    Update pattern.cpp

--- Roadmap_to_CPP/Foundations/pattern.cpp
+++ Roadmap_to_CPP/Foundations/pattern.cpp
@@ -23,4 +23,102 @@ OUTPUT:
 *****
 
 **/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/**
+1
+12
+123
+1234
+12345
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/**
+1
+22
+333
+4444
+55555
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 5; j >= i; j--)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/**
+[?2004l
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+[?2004h
+
+**/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int j = 5; j >= i; j--)
+        {
+            cout<<5 - j + 1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
+/**
+[?2004l
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+[?2004h
+**/
 
