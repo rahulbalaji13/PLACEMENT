357fc2ffe48a728e9bd2fcf1e7a96ab71bffd736
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2026-01-10 17:10:40 +0530

    Update smallestSubTreeAllDeepnodes.py

--- LeetCode/DCCJan2026/smallestSubTreeAllDeepnodes.py
+++ LeetCode/DCCJan2026/smallestSubTreeAllDeepnodes.py
@@ -1,3 +1,20 @@
 class Solution(object):
     def subtreeWithAllDeepest(self, root):
-        
+        depth = {None : -1}
+        def dfs(node, parent = None):
+            if node:
+                dfs(node.left, node)
+                dfs(node.right, node)
+
+        dfs(root)
+
+        max_depth = max(depth.iteratevalues())
+
+        def answer():
+            if not node or depth.get(node, None) == max_depth:
+                return node
+            L, R = answer(node.left), answer(node.right)
+            return node  if L and R else L or R
+
+        return answer(root)
+            
