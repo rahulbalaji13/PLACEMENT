b0fa67d020d5c4017be7199d81d1367e640f59f0
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2026-02-01 22:10:14 +0530

    Update DivideArrToSubarrWithMinCost.py

--- LeetCode/DCCFeb2026/DivideArrToSubarrWithMinCost.py
+++ LeetCode/DCCFeb2026/DivideArrToSubarrWithMinCost.py
@@ -1,1 +1,7 @@
-
+class Solution(object):
+    def minimumCost(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return nums[0] + sum(sorted(nums[1:]) [:2])
