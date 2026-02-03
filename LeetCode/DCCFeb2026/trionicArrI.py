70309f224404e7f83b3e1ea7ae439fe5de9c877d
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2026-02-03 21:31:40 +0530

    Update trionicArrI.py

--- LeetCode/DCCFeb2026/trionicArrI.py
+++ LeetCode/DCCFeb2026/trionicArrI.py
@@ -1,1 +1,25 @@
+class Solution(object):
+    def isTrionic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        i = 1
 
+        # For Increasing
+        while i < n and nums[i - 1] < nums[i]:
+            i += 1
+        p = i - 1
+
+        # For Decresing 
+        while i < n and nums[i - 1] > nums[i]:
+            i += 1
+        q = i - 1
+
+        # For Increasing
+        while i < n and nums[i - 1] < nums[i]:
+            i += 1
+        flag = i - 1
+
+        return (p != 0 and q != p) and (flag == n - 1 and flag != q)
