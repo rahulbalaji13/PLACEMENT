76b9829c393b6882e7c22c717fae9fd002748510
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2026-02-08 00:02:59 +0530

    Update minRemovalToBalenceArr.py

--- LeetCode/DCCFeb2026/minRemovalToBalenceArr.py
+++ LeetCode/DCCFeb2026/minRemovalToBalenceArr.py
@@ -1,1 +1,20 @@
+class Solution(object):
+    def minRemoval(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(nums)
+        nums.sort()
 
+        ans = n
+        right = 0
+
+        for left in range(n):
+            while right < n and nums[right] <= nums[left] * k:
+                right += 1
+
+            ans = min(ans, n - (right - left))
+
+        return ans
