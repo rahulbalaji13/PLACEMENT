9419dfaf51498bff9d16b86db4861b28513d65b6
RAHUL BALAJI <rahul.balaji13289@gmail.com>
2026-02-07 23:58:05 +0530

    Update minDeletionToMakeStrBalenced.py

--- LeetCode/DCCFeb2026/minDeletionToMakeStrBalenced.py
+++ LeetCode/DCCFeb2026/minDeletionToMakeStrBalenced.py
@@ -1,1 +1,20 @@
+# Using stack approach
 
+class Solution(object):
+    def minimumDeletions(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        char_stack = []
+        delete_cnt = 0
+
+        for char in s:
+            if char_stack and char_stack[-1] == "b" and char == "a":
+                char_stack.pop()
+                delete_cnt += 1
+            else:
+                char_stack.append(char)
+
+        return delete_cnt
+        
